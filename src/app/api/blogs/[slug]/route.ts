5335b3fdc82b47a8ba4ac8513ad6850380a73a40
haroonkpk <haroonkhanlala47@gmail.com>
2025-10-10 12:58:20 +0500

    Refactor GET handler in blog route: improve code formatting for better readability and maintainability.

--- src/app/api/blogs/[slug]/route.ts
+++ src/app/api/blogs/[slug]/route.ts
@@ -2,15 +2,24 @@ import { NextRequest, NextResponse } from "next/server";
 import Blog from "@/models/blog.model";
 import connectDB from "@/lib/mongodb";
 
-export async function GET(request: NextRequest, { params }: { params: { slug: string } }) {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { slug: string } }
+) {
   try {
     await connectDB();
+
     const blog = await Blog.findOne({ slug: params.slug });
+
     if (!blog) {
       return NextResponse.json({ message: "Blog not found" }, { status: 404 });
     }
+
     return NextResponse.json(blog, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ message: "Error fetching blog", error }, { status: 500 });
+    return NextResponse.json(
+      { message: "Error fetching blog", error },
+      { status: 500 }
+    );
   }
 }
